1fb164240663dbb8d8502d51da850b0bb8d686ba
mlatman <mlatman@gmail.com>
2018-02-17 19:46:06 -0500

    All pods

--- ios/ProjectTune/ProjectTune/AddSongTableViewController.swift
+++ ios/ProjectTune/ProjectTune/AddSongTableViewController.swift
@@ -24,7 +24,7 @@ class AddSongTableViewController: UITableViewController, UISearchBarDelegate {
         searchController.searchResultsDataSource = self
         
        
-        //searchController.searchResultsTableView.color
+        //searc
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -34,6 +34,7 @@ class AddSongTableViewController: UITableViewController, UISearchBarDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         CRNotifications.showNotification(type: .success, title: "Added Song", message: "Keep the tunes going!", dismissDelay: 3)
+        PNFirebase.add(track: model.tracks![indexPath.row], broadcastId: "testing")
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
